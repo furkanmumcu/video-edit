a1ed00618de4a880ec274e6e3c05b1c3e4198778
Furkan Mumcu <furkanmumcu2@gmail.com>
2022-02-09 11:40:55 -0500

    initial commit

---
+++ utils.py
@@ -0,0 +1,18 @@
+
+
+#input: video name
+#output: numpy array that contains frames of video
+
+
+#input: numpy array contains frames
+#output: creates video
+
+
+#input: video name
+#output: video specs: frameCount, frameWidth, frameHeight, channel, fps, fourcc_string
+
+
+#function to remove/replace frames to create frozen effect
+
+
+#funtion to lower frame quality
