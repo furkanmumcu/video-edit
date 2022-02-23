40ec030cbd45a71de67658cb6d691cf462a8589c
Furkan Mumcu <furkanmumcu2@gmail.com>
2022-02-22 20:31:54 -0500

    update

--- vid_split.py
+++ vid_split.py
@@ -6,11 +6,9 @@ def vid_to_jpg(vid_name):
 	vid_name_ext = vid_name + '.avi'
 	vid_frames = utils.vid_to_frames(vid_name_ext)
 
+	digits = len(str(vid_frames.shape[0]))
 	for i in range(vid_frames.shape[0]):
-		if i < 10:
-			frame_index_str = '0' + str(i)
-		else:
-			frame_index_str = str(i)
+		frame_index_str = '0' * (digits - len(str(i))) + str(i)
 
 		path = vid_name + '/'
 		frame_name = frame_index_str + ".jpg"
@@ -25,7 +23,7 @@ def vids_to_jpg():
 			vid_to_jpg(str(i))
 
 
-#vidname = '01'
+#vidname = '10'
 #vid_to_jpg(vidname)
 
 vids_to_jpg()
